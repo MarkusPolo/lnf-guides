83d76efc5194efd67e6bf9f214e644b47a59e9e5
MarkusPolo <Marius.Morg@gmail.com>
2025-09-01 00:41:16 +0200

    Newsletterbar, Consent, Comments, AdSlots

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,16 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare global {
+  interface Window {
+    consent?: {
+      get: () => { ads: boolean; external: boolean; ts: number };
+      set: (p: Partial<{ ads: boolean; external: boolean }>) => void;
+      setAll: (v: boolean) => void;
+      decided: () => boolean;
+      KEY: string;
+    };
+  }
+}
+export {};
+
